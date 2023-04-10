4c6ddcb2dc928305eca256a132134bc951de57b4
Erkut Güler <erkut131@gmail.com>
2023-04-10 16:41:10 +0300

    operatorler örnek

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayıyı Giriniz: ");
+            int Sayi = Convert.ToInt32(Console.ReadLine());
+            for (int i = Sayi; i > 10 ; i/=2  )
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
